c6cfb79cd44eadb826c71b540bbf311d9dabb167
Ranniery Moreira <moreiraranni@gmail.com>
2021-03-17 15:41:47 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          600: "#343a3f"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
